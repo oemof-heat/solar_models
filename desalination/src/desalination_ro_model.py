92b36451d5a62c5d4ec4e08bd67f8cf4ddc12fd7
caroline.moeller <caroline.moeller@rl-institut.de>
2020-10-01 16:14:47 +0200

    Fix data read

--- desalination/src/desalination_ro_model.py
+++ desalination/src/desalination_ro_model.py
@@ -114,7 +114,7 @@ def run_model_ro(config_path, var_number):
     pv = solph.Source(
         label='pv_source',
         outputs={bele: solph.Flow(
-            fix=data['global_horinzontal_in_W_m2'],
+            fix=data['global_horizontal_relative'],
             investment=solph.Investment(
                 ep_costs=ep_costs_f(
                     param_value['pv_invest_costs_output_el'],
